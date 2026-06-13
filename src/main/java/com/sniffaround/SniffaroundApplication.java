e6dd6fe34781e3e53eb31d4349b61c32065705c0
MidhunManu <midhunmanu384@gmail.com>
2026-06-13 23:21:31 +0530

    init repository

---
+++ src/main/java/com/sniffaround/SniffaroundApplication.java
@@ -0,0 +1,13 @@
+package com.sniffaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffaroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SniffaroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sniffaround/SniffaroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sniffaround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SniffaroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
