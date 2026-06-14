2f12af7a320e4597c77ad64ff459d945e0c2e3d0
MidhunManu <midhunmanu384@gmail.com>
2026-06-14 09:55:02 +0530

    scaffold the app

--- src/main/java/com/sniffaround/SniffaroundApplication.java
+++ src/main/java/com/sniffaround/SniffaroundApplication.java
@@ -2,8 +2,10 @@ package com.sniffaround;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SniffaroundApplication {
 
     public static void main(String[] args) {
