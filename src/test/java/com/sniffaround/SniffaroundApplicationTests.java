9df656ca9d443be09f728df4bf2bc2391b65eb07
MidhunManu <midhunmanu384@gmail.com>
2026-06-22 19:17:29 +0530

    add: configure test

--- src/test/java/com/sniffaround/SniffaroundApplicationTests.java
+++ src/test/java/com/sniffaround/SniffaroundApplicationTests.java
@@ -1,9 +1,14 @@
 package com.sniffaround;
 
+import com.sniffaround.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@Import(TestConfig.class)
+@ActiveProfiles("test")
 class SniffaroundApplicationTests {
 
     @Test
