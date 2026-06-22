bc05cfb8390eeabe2c380adbaf3d4a86c862ef7a
MidhunManu <midhunmanu384@gmail.com>
2026-06-22 19:22:38 +0530

    add: make minio service primary for test

--- src/test/java/com/sniffaround/config/TestConfig.java
+++ src/test/java/com/sniffaround/config/TestConfig.java
@@ -4,10 +4,12 @@ import com.sniffaround.Service.MinioService;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @TestConfiguration
 public class TestConfig {
     @Bean
+    @Primary
     public MinioService minioService() {
         return Mockito.mock(MinioService.class);
     }
