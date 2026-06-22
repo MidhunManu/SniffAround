e151b2a53978caef4af1573a81823a0da308823e
MidhunManu <midhunmanu384@gmail.com>
2026-06-22 19:20:12 +0530

    fix(Test): make bean for mocked service

--- src/test/java/com/sniffaround/config/TestConfig.java
+++ src/test/java/com/sniffaround/config/TestConfig.java
@@ -3,9 +3,11 @@ package com.sniffaround.config;
 import com.sniffaround.Service.MinioService;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
 public class TestConfig {
+    @Bean
     public MinioService minioService() {
         return Mockito.mock(MinioService.class);
     }
