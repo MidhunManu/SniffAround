4a390a0f3fd58372f7f732138a4a25c2e46e6d57
MidhunManu <midhunmanu384@gmail.com>
2026-06-24 20:27:32 +0530

    feat: thymleaf template for emails

--- src/main/java/com/sniffaround/Service/MailService.java
+++ src/main/java/com/sniffaround/Service/MailService.java
@@ -9,40 +9,33 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class MailService implements NotificationService {
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
     @Value("${spring.mail.username}")
     private String from;
 
     @Async
     @Override
     public void send(String to, String subject, String body) throws MessagingException {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//
-//        message.setFrom(from);
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(body);
-//
-//        mailSender.send(message);
+        Context context = new Context();
+        context.setVariables(Map.of("name", "user", "email", to));
 
+        String html = this.templateEngine.process("emails/welcome", context);
         MimeMessage message = this.mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(to);
         helper.setSubject(subject);
-        String html = """
-                <html>
-                    <body>
-                        <h1>Welcome</h1>
-                        <p>Your account has been created</p>
-                    </body>
-                </html>
-                """;
         helper.setText(html, true);
-        mailSender.send(message);
+        this.mailSender.send(message);
     }
 }
