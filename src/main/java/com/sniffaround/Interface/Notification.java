e18c0d370d2215b43f696ff8474bba313549e636
MidhunManu <midhunmanu384@gmail.com>
2026-06-25 10:33:51 +0530

    refactor: remove unused annotation and access modifier

--- src/main/java/com/sniffaround/Interface/Notification.java
+++ src/main/java/com/sniffaround/Interface/Notification.java
@@ -1,11 +1,9 @@
 package com.sniffaround.Interface;
 
 import jakarta.mail.MessagingException;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface Notification {
-    public void sendEmail(String to) throws MessagingException;
-    default public void sendPushNotification() {}
-    default public void sendSMS() {}
+    void sendEmail(String to) throws MessagingException;
+    default void sendPushNotification() {}
+    default void sendSMS() {}
 }
